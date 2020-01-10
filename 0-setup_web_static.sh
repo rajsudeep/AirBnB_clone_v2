f0ae946d880a0359e8ed35b1f4a9d0b9c4811128
rajsudeep <sudrocks@gmail.com>
2020-01-10 17:10:48 +0000

    Update perms

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #sets up a server for deployment of web_static
 
-apt-get update
-apt-get -y install nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/releases/shared
-echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '41i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/{releases/test,shared}
+sudo chown -R $USER:$USER /data/web_static/releases/test/
+sudo echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx start

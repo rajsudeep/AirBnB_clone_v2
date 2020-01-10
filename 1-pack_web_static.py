8d7aa865c7d42400d282a98f1ad8c0bf312a7432
rajsudeep <sudrocks@gmail.com>
2020-01-10 21:04:30 +0000

    Retry

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,15 +3,17 @@
 from fabric.api import local
 from datetime import datetime
 
+
 n = datetime.now()
+
 
 def do_pack():
     """Packs web_static files into .tgz file"""
 
-    time = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive = 'versions/web_static_' + time + '.tgz'
+    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
     local('mkdir -p versions')
-    command = local("mv " + archive + " ./versions/")
+    command = local("tar -cvzf " + file_name + " ./web_static/")
     if command.succeeded:
-        return archive
+        return file_name
     return None

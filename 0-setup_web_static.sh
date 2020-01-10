202331e5f2205400edf408ae466e24fd4a5eeac6
rajsudeep <sudrocks@gmail.com>
2020-01-10 17:19:49 +0000

    Remove sudo from commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # Sets up a server for deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo chown -R $USER:$USER /data/web_static/releases/test/
-sudo echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-sudo service nginx start
-exit 0
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/{releases/test,shared}
+chown -R "$USER":"$USER" /data/web_static/releases/test/
+echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx start

108e7b60f58c9d35489de035d3ed0a6329158f1f
rajsudeep <sudrocks@gmail.com>
2020-01-10 21:16:48 +0000

    Refactor attempt 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,16 +4,13 @@ from fabric.api import local
 from datetime import datetime
 
 
-n = datetime.now()
-
-
 def do_pack():
     """Packs web_static files into .tgz file"""
 
-    file_name = 'versions/web_static_{}{}{}{}{}{}.tgz'\
-                .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive = 'versions/web_static_' + time + '.tgz'
     local('mkdir -p versions')
-    command = local("tar -cvzf " + file_name + " ./web_static/")
+    command = local("tar -cvzf " + archive + " ./web_static/")
     if command.succeeded:
-        return file_name
+        return archive
     return None

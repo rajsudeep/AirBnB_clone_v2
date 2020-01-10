46049825b89520d81d66ae1035f58cb9d1b311a6
rajsudeep <sudrocks@gmail.com>
2020-01-10 17:26:40 +0000

    Shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# Sets up a server for deployment of web_static
 
 apt-get update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/{releases/test,shared}
-chown -R "$USER":"$USER" /data/web_static/releases/test/
-echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+echo -e "<html>\n\t<head>\n\t\tHolberton School\n\t</head>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default

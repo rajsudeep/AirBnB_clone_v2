4e525ceb9bbafe5df361e48575820be34ee2d99f
rajsudeep <sudrocks@gmail.com>
2020-01-20 20:48:18 +0000

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Says hello """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Says HBNB """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display C followed by text """
+    return "C " + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ Display Python followed by text """
+    return "Python " + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

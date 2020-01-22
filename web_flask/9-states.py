d70c334f9ac5a04c844551d40eb429d3d1754fa7
rajsudeep <sudrocks@gmail.com>
2020-01-22 19:14:47 +0000

    Pep8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,6 +4,7 @@ from models import storage
 from models.state import State
 app = Flask(__name__)
 
+
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def state_id(id=None):

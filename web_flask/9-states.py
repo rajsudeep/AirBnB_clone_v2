8839f6afc8152d3fad49d605790cde7b924bb67c
rajsudeep <sudrocks@gmail.com>
2020-01-22 19:10:47 +0000

    Add task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id=None):
+    """ Display state """
+    state = None
+    states = storage.all(state)
+    if not id:
+        state_list = list(states.values())
+    else:
+        if ("State." + id) in states.keys():
+            state = states["State." + id]
+    return render_template('9-states.html', **locals())
+
+
+@app.teardown_appcontext
+def close(self):
+    """ Close app """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
